9ad35929d58e3ef3703d6726cd09041ed3a5ff10
Shygee <cole.w.bowman06@gmail.com>
2025-04-13 13:54:45 -0500

    here you go funky

--- example_mods/jcwtg/stages/reno.hx
+++ example_mods/jcwtg/stages/reno.hx
@@ -3,6 +3,8 @@ import states.PlayState;
 var reno:FlxSprite;
 var gummy:FlxSprite;
 var stupid = game.createRuntimeShader("ripplw");
+var midX = FlxG.width / 2;
+var midY = (FlxG.height / 2) + 200;
 function onCreate() {
     reno = new FlxSprite(0, 0).loadGraphic(Paths.image("reno"));
     gummy = new FlxSprite(0, 0).loadGraphic(Paths.image("gummy"));
@@ -10,14 +12,31 @@ function onCreate() {
     gummy.screenCenter();
     reno.cameras = [camGame];
     gummy.cameras = [camGame];
-    reno.scale.set(1.8, 1.8);
-    gummy.scale.set(1.8, 1.8);
+    reno.scale.set(2.2, 2.2);
+    gummy.scale.set(2.2, 2.2);
     reno.scrollFactor.set(1, 1);
     gummy.scrollFactor.set(1, 1);
     addBehindDad(reno);
     addBehindDad(gummy);
     gummy.alpha = 0;
     gummy.shader = stupid;
+}
+
+function centerCamera(?poop:Float) {
+    if (poop != null) {
+        triggerEvent('Camera Follow Pos', midX, midY - poop);
+    }
+    else 
+    {
+        triggerEvent('Camera Follow Pos', midX, midY);
+    }
+}
+
+function onCreatePost() {
+    centerCamera();
+
+    game.boyfriendGroup.visible = false;
+    game.dadGroup.visible = false;
 }
 
 function onBeatHit() {
