e5bf967fbe0998de41789c2eb0e6a5985f8d6d3b
fuknypopo <120442115+funkypopp@users.noreply.github.com>
2025-04-16 23:45:35 -0700

    okay

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -25,9 +25,7 @@ class MainMenuState extends MusicBeatState
 
 	//Centered/Text options
 	var optionShit:Array<String> = [
-		'story_mode',
 		'freeplay',
-		#if MODS_ALLOWED 'mods', #end
 		'credits'
 	];
 
