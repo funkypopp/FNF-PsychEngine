2228aa9bc293d7eb796e464066e8964c91ddf179
data5 <99661153+FixedData@users.noreply.github.com>
2025-04-21 23:03:40 -0600

    Update circle thing.hx

--- example_mods/pearleon/data/my-first-song/circle thing.hx
+++ example_mods/pearleon/data/my-first-song/circle thing.hx
@@ -8,7 +8,7 @@ function createCircle(dataNum:Int, char:String) {
 	var thickness = 15;
 	var diameter = radius * 2 + thickness;
 
-	var cirSprite = new Circle(0,0,diameter,diameter,noteColors[dataNum]);
+	var cirSprite = new Circle(0,0,radius,radius,noteColors[dataNum]);
 	if (char == 'dad') cirSprite.setPosition(FlxG.random.int(-450, 125), FlxG.random.int(0, 720));
 	else if (char == 'bf') cirSprite.setPosition(FlxG.random.int(50, 550), FlxG.random.int(0, 720));
 
@@ -18,7 +18,7 @@ function createCircle(dataNum:Int, char:String) {
 	insert(members.indexOf(game.getLuaObject("shadows")), cirSprite);
 
 
-	FlxTween.num(1, 1.2, 1.1, { ease: FlxEase.expoOut },
+	FlxTween.num(1, 1.5, 1.1, { ease: FlxEase.expoOut },
 		(r) -> {
 			cirSprite.scale.set(r,r);
 			// cirSprite.fill(FlxColor.TRANSPARENT);
